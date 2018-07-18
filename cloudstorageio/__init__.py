a09b5082eac2c95d62b855cecfaaeafedf00f119
jgoutin <jgoutin@accelize.com>
2018-07-18 15:35:28 +0200

    Basic S3 raw IO

--- cloudstorageio/__init__.py
+++ cloudstorageio/__init__.py
@@ -1,5 +1,14 @@
+# coding=utf-8
+"""Cloud storage I/O"""
+
+from abc import abstractmethod as _abstractmethod
 import io as _io
 import os as _os
+
+# TODO: OSError for function depending on seekable, writable, ...
+# TODO: Check full exception, and IO interface behavior
+
+# Generic functions to implement
 
 
 def open():
@@ -10,69 +19,184 @@ def copy():
     """"""
 
 
-def getsize():
+def getsize(path):
+    """
+    Return the size, in bytes, of path.
+
+    Returns:
+        int: Size in bytes.
+
+    Raises:
+         OSError: if the file does not exist or is inaccessible.
+    """
+
+
+def getmtime(path):
+    """
+    Return the time of last access of path.
+
+    Returns:
+        float: The number of seconds since the epoch
+            (see the time module).
+
+    Raises:
+         OSError: if the file does not exist or is inaccessible.
+    """
+
+
+def listdir(self):
     """"""
 
+
+# Storage classes
 
 class RawStorageIO(_io.RawIOBase):
     """Raw binary storage I/O"""
 
-    def __init__(self, name, mode='w+'):
+    def __init__(self, name, mode='r'):
         _io.RawIOBase.__init__(self)
         self._name = name
-        self._mode = mode  # r,w,x,+
+        self._mode = mode
         self._seek = 0
-        self._size = 0
+        self._seekable = True
 
-    def flush(self):
-        """"""
+        if mode == 'w':
+            self._write_buffer = bytearray()
+            self._readable = False
+            self._writable = True
+        elif mode == 'r':
+            self._readable = True
+            self._writable = False
+        else:
+            raise ValueError(
+                'Unsupported mode "%s"' % mode)
+
+    @_abstractmethod
+    def getsize(self):
+        """
+        Return the size, in bytes, of path.
+
+        Returns:
+            int: Size in bytes.
+
+        Raises:
+             OSError: if the file does not exist or is inaccessible.
+        """
+
+    @_abstractmethod
+    def getmtime(self):
+        """
+        Return the time of last access of path.
+
+        Returns:
+            float: The number of seconds since the epoch
+                (see the time module).
+
+        Raises:
+             OSError: if the file does not exist or is inaccessible.
+        """
 
     @property
     def mode(self):
-        """"""
+        """
+        The mode.
+
+        Returns:
+            str: Mode.
+        """
         return self._mode
 
     @property
     def name(self):
-        """"""
+        """
+        The file name.
+
+        Returns:
+            str: Name.
+        """
         return self._name
 
-    def readall(self):
-        """"""
-
     def readable(self):
-        """"""
-        return 'b' in self._mode or '+' in self._mode
+        """
+        Return True if the stream can be read from.
+        If False, read() will raise OSError.
 
-    def readinto(self, b):
-        """"""
+        Returns:
+            bool: Supports reading.
+        """
+        return self._readable
 
     def seek(self, offset, whence=_os.SEEK_SET):
-        """"""
+        """
+        Change the stream position to the given byte offset.
+
+        Args:
+            offset: Offset is interpreted relative to the position indicated by whence.
+            whence: The default value for whence is SEEK_SET. Values for whence are:
+                SEEK_SET or 0 – start of the stream (the default);
+                offset should be zero or positive
+                SEEK_CUR or 1 – current stream position;
+                offset may be negative
+                SEEK_END or 2 – end of the stream;
+                offset is usually negative
+
+        Returns:
+            int: The new absolute position.
+        """
         if whence == _os.SEEK_SET:
             self._seek = offset
         elif whence == _os.SEEK_CUR:
             self._seek += offset
         elif whence == _os.SEEK_END:
-            self._seek = offset + self._size
-
-    @staticmethod
-    def seekable():
-        """"""
-        return True
-
-    def tell(self):
-        """"""
+            self._seek = offset + self.getsize()
         return self._seek
 
-    def writable(self):
-        """"""
-        return 'w' in self._mode
+    def seekable(self):
+        """
+        Return True if the stream supports random access.
+        If False, seek(), tell() and truncate() will raise OSError.
+
+        Returns:
+            bool: Supports random access.
+        """
+        return self._seekable
+
+    def tell(self):
+        """Return the current stream position.
+
+        Returns:
+            int: Stream position."""
+        return self._seek
 
     def write(self, b):
-        """"""
+        """
+        Write the given bytes-like object, b, to the underlying raw stream,
+        and return the number of bytes written.
 
-# getbuffer, getvalue
+        Args:
+            b (bytes-like object): Bytes to write.
+
+        Returns:
+            int: The number of bytes written.
+        """
+        # This function write data in a buffer
+        # This buffer need to be flushed to write content on
+        # Cloud Storage
+        size = len(b)
+        start = self._seek
+        self._seek += size
+        self._write_buffer[start:self._seek] = b
+        return size
+
+    def writable(self):
+        """
+        Return True if the stream supports writing.
+        If False, write() and truncate() will raise OSError.
+
+        Returns:
+            bool: Supports writing.
+        """
+        return self._writable
 
 
 class BufferedStorageIO(_io.BufferedIOBase):
---
+++ cloudstorageio/s3.py
@@ -0,0 +1,141 @@
+# coding=utf-8
+"""Amazon Web Services S3"""
+
+import boto3 as _boto3
+from botocore.exceptions import ClientError as _ClientError
+
+from cloudstorageio import RawStorageIO as _RawStorageIO
+
+
+# TODO: 404 error handling
+
+
+class RawS3IO(_RawStorageIO):
+    """Raw binary S3 I/O"""
+
+    def __init__(self, name, mode='r', **boto3_session_args):
+
+        # Splits URL scheme is any
+        try:
+            path = name.split('://')[1]
+        except IndexError:
+            path = name
+            name = 's3://' + path
+
+        # Initializes storage
+        _RawStorageIO.__init__(self, name, mode)
+
+        # Instantiates S3 client
+        self._client = _boto3.session.Session(
+            **boto3_session_args).client("s3")
+
+        # Prepares S3 I/O functions and arguments
+        self._get_object = self._client.get_object
+        self._put_object = self._client.put_object
+        self._head_object = self._client.head_object
+
+        bucket_name, key = path.split('/', 1)
+        self._client_kwargs = dict(Bucket=bucket_name, Key=key)
+
+    def _get_metadata(self):
+        """
+        Returns object metadata.
+
+        Returns:
+            dict: Object metadata.
+        """
+        return self._head_object(**self._client_kwargs)
+
+    def getsize(self):
+        """
+        Return the size, in bytes, of path.
+
+        Returns:
+            int: Size in bytes.
+
+        Raises:
+             OSError: if the file does not exist or is inaccessible.
+        """
+        return self._get_metadata()['ContentLength']
+
+    def getmtime(self):
+        """
+        Return the time of last access of path.
+
+        Returns:
+            float: The number of seconds since the epoch
+                (see the time module).
+
+        Raises:
+             OSError: if the file does not exist or is inaccessible.
+        """
+        return self._get_metadata()['LastModified'].timestamp()
+
+    def readinto(self, b):
+        """
+        Read bytes into a pre-allocated, writable bytes-like object b,
+        and return the number of bytes read.
+
+        Args:
+            b (bytes-like object): buffer.
+
+        Returns:
+            int: number of bytes read
+        """
+        # Get object part from S3
+        try:
+            response = self._get_object(
+                Range='bytes=%d-%d' % (
+                    self._seek, self._seek + len(b) - 1),
+                **self._client_kwargs)
+
+        # Check for end of file
+        except _ClientError as exception:
+            error_code = exception.response['Error']['Code']
+            if error_code == 'InvalidRange':
+                # EOF
+                return 0
+            raise
+
+        # Get object content
+        body = response['Body'].read()
+        body_size = len(body)
+        b[:body_size] = body
+
+        # Update stream position
+        self._seek += body_size
+        return body_size
+
+    def readall(self):
+        """
+        Read and return all the bytes from the stream until EOF.
+
+        Returns:
+            bytes: Object content
+        """
+        # Get object starting from seek
+        if self._seek:
+            response = self._get_object(
+                Range='bytes=%d-' % self._seek,
+                **self._client_kwargs)
+
+        # Get object starting from object start
+        else:
+            response = self._get_object(
+                **self._client_kwargs)
+
+        # Get object content
+        body = response['Body'].read()
+
+        # Update stream position
+        self._seek += len(body)
+        return body
+
+    def flush(self):
+        """
+        Flush the write buffers of the stream if applicable.
+        """
+        # This send to S3 the entire file
+        self._put_object(
+            Body=memoryview(self._write_buffer).tobytes(),
+            **self._client_kwargs)
