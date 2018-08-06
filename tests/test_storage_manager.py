fc8d200b1ea2d4186b8aed142a668ded26807427
jgoutin <jgoutin@accelize.com>
2018-08-06 10:49:43 +0200

    Speed up test

--- tests/test_storage_manager.py
+++ tests/test_storage_manager.py
@@ -20,6 +20,7 @@ def test_register():
     from pycosio._core.storage_manager import register, STORAGE, get_instance
     from pycosio.storage.http import (
         HTTPRawIO, _HTTPSystem, HTTPBufferedIO)
+    import requests
 
     # Get HTTP as storage to register
     prefixes = _HTTPSystem().prefixes
@@ -28,70 +29,96 @@ def test_register():
         raw=HTTPRawIO, system=_HTTPSystem,
         buffered=HTTPBufferedIO,
         storage_parameters=storage_parameters)
-    url = 'https://raw.githubusercontent.com/Accelize/pycosio/master/README.md'
+    https = 'https://pass'
+    http = 'http://pass'
+
+    # Mock requests
+    class Response:
+        """Fake response"""
+        status_code = 200
+        headers = {'Accept-Ranges': 'bytes',
+                   'Content-Length': '100',}
+
+    class Session:
+        """Fake Session"""
+        def __init__(self, *_, **__):
+            """Do nothing"""
+
+        @staticmethod
+        def request(*_, **__):
+            """Returns fake result"""
+            return Response()
+
+    requests_session = requests.Session
+    requests.Session = Session
 
     # Tests register:
-    for register_kwargs in (
-            # With storage
-            dict(storage='http',
-                 storage_parameters=storage_parameters),
-            # With name
-            dict(name='http://path',
-                 storage_parameters=storage_parameters),
-            # With both
-            dict(storage='http', name='http://path',
-                 storage_parameters=storage_parameters),
-            # Lazy registration
-            None):
+    try:
+        for register_kwargs in (
+                # With storage
+                dict(storage='http',
+                     storage_parameters=storage_parameters),
+                # With name
+                dict(name=http,
+                     storage_parameters=storage_parameters),
+                # With both
+                dict(storage='http', name=http,
+                     storage_parameters=storage_parameters),
+                # Lazy registration
+                None):
 
-        # Unregister if already registered
-        for prefix in prefixes:
-            try:
+            # Unregister if already registered
+            for prefix in prefixes:
+                try:
+                    del STORAGE[prefix]
+                except KeyError:
+                    continue
+
+            # Add dummy storage to registered
+            STORAGE['aaaa'] = {}
+            STORAGE['zzzz'] = {}
+
+            # Register
+            if register_kwargs:
+                # Using register function
+                register(**register_kwargs)
+            else:
+                # Using lazy registration
+                get_instance(name=http,
+                             storage_parameters=storage_parameters)
+
+            # Check registration
+            for prefix in prefixes:
+                # Test infos
+                for key, value in expected_info.items():
+                    assert STORAGE[prefix][key] == value
+
+                # Tests cached system
+                assert isinstance(
+                    STORAGE[prefix]['system_cached'], _HTTPSystem)
+
+                # Tests get_instance cached system
+                assert get_instance(
+                    name=prefix) is STORAGE[prefix]['system_cached']
+
+                # Test get_instance other classes with cached system
+                raw = get_instance(name=https, cls='raw')
+                assert isinstance(raw, HTTPRawIO)
+                assert raw._system is STORAGE[prefix]['system_cached']
+
+                buffered = get_instance(name=http, cls='buffered')
+                assert isinstance(buffered, HTTPBufferedIO)
+                assert buffered._system is STORAGE[prefix]['system_cached']
+
+            # Test register order
+            assert tuple(STORAGE) == tuple(reversed(sorted(STORAGE)))
+
+            # Cleanup
+            del STORAGE['aaaa']
+            del STORAGE['zzzz']
+            for prefix in prefixes:
                 del STORAGE[prefix]
-            except KeyError:
-                continue
 
-        # Add dummy storage to registered
-        STORAGE['aaaa'] = {}
-        STORAGE['zzzz'] = {}
-
-        # Register
-        if register_kwargs:
-            # Using register function
-            register(**register_kwargs)
-        else:
-            # Using lazy registration
-            get_instance(name='http://path',
-                         storage_parameters=storage_parameters)
-
-        # Check registration
-        for prefix in prefixes:
-            # Test infos
-            for key, value in expected_info.items():
-                assert STORAGE[prefix][key] == value
-
-            # Tests cached system
-            assert isinstance(
-                STORAGE[prefix]['system_cached'], _HTTPSystem)
-
-            # Tests get_instance cached system
-            assert get_instance(
-                name=prefix) is STORAGE[prefix]['system_cached']
-
-            # Test get_instance other classes with cached system
-            raw = get_instance(name=url, cls='raw')
-            assert isinstance(raw, HTTPRawIO)
-            assert raw._system is STORAGE[prefix]['system_cached']
-
-            buffered = get_instance(name=url, cls='buffered')
-            assert isinstance(buffered, HTTPBufferedIO)
-            assert buffered._system is STORAGE[prefix]['system_cached']
-
-        # Test register order
-        assert tuple(STORAGE) == tuple(reversed(sorted(STORAGE)))
-
-        # Cleanup
-        del STORAGE['aaaa']
-        del STORAGE['zzzz']
-        for prefix in prefixes:
-            del STORAGE[prefix]
+    # Restore mocked functions
+    finally:
+        requests.Session = requests_session
