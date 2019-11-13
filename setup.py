acf2f0273a71b984542edff7b1337b3ca80550ab
jgoutin <jgoutin@accelize.com>
2019-11-13 15:38:19 +0100

    Pin "azure-storage-blob" version because API breaking changes.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ PACKAGE_INFO = dict(
     install_requires=['requests>=2.20.0', 'python-dateutil>=2.6.0'],
     extras_require={
         # Storage specific requirements
-        'azure_blob': ['azure-storage-blob>=1.3.0'],
+        'azure_blob': ['azure-storage-blob>=1.3.0,<=2.1.0'],
         'azure_file': ['azure-storage-file>=1.3.0'],
         'oss': ['oss2>=2.3.0'],
         's3': ['boto3>=1.5.0'],
