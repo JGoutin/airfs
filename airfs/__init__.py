334edc5488eff7abfb9138e8377870a097ae6e45
jgoutin <jgoutin@pm.me>
2020-07-21 23:21:17 +0200

    Increase version + license as comment

--- airfs/__init__.py
+++ airfs/__init__.py
@@ -1,21 +1,21 @@
-"""Python "Remote File Systems" library
+"""Python "Remote File Systems" library"""
 
-Copyright 2020 J.Goutin
-Copyright 2018-2019 Accelize (As the "pycosio" library)
+# Copyright 2020 J.Goutin
+# Copyright 2018-2019 Accelize (As the "pycosio" library)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-__version__ = "1.4.0"
+__version__ = "1.5.0"
 __copyright__ = "Copyright 2020 J.Goutin"
 __licence__ = "Apache 2.0"
 
